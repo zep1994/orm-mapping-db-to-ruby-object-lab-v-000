4617d648b5e24f151cf5a4f239ef509b9865e81b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 06:51:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
       self.new_from_db(row)
     end
   end
-  
+
    #  def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
