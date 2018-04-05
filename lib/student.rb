8b2ff5bdb7176c4ee3a0692bae20a6fcebc08e0d
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 07:05:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,14 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = #{x}
+      SQL
+      DB[:conn].execute(sql)
+  end
+
   def self.first_student_in_grade_10
   self.new_from_db(DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1")[0])
   end
