be4cbc0c212d85030642a5f93421593613218b1c
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 06:40:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,18 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    def self.find_by_name(name)
+  sql = <<-SQL
+    SELECT *
+    FROM songs
+    WHERE name = ?
+    LIMIT 1
+  SQL
+
+  DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
+end
   end
 
   def save
