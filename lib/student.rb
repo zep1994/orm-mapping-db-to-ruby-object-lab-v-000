c9bc13c9dd86f6d960d61619b1c449d6862c83c7
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 06:56:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,15 @@ class Student
       DB[:conn].execute(sql)
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE  grade < 12
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
